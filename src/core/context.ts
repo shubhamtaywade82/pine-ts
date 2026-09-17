cd1971cafe898d54f1671275a9330ab0ae595267
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-17 07:58:25 +0530

    feat(core): expose Pine persistent state through context

--- src/core/context.ts
+++ src/core/context.ts
@@ -1,5 +1,6 @@
 import type { Bar, BarState, SymbolInfo } from "./types.js";
 import { createSeries, type Series } from "./series.js";
+import type { PineState } from "./state.js";
 
 export interface PineContext {
   readonly bar: Bar;
@@ -14,32 +15,27 @@ export interface PineContext {
   readonly ohlc4: Series<number>;
   readonly barstate: BarState;
   readonly syminfo: SymbolInfo;
+  readonly state: PineState;
 }
 
 export class OhlcvSeries {
-  public readonly open = createSeries<number>();
-  public readonly high = createSeries<number>();
-  public readonly low = createSeries<number>();
-  public readonly close = createSeries<number>();
-  public readonly volume = createSeries<number>();
-  public readonly time = createSeries<number>();
-  public readonly hl2 = createSeries<number>();
-  public readonly hlc3 = createSeries<number>();
+  public readonly open = createSeries<number>(); public readonly high = createSeries<number>();
+  public readonly low = createSeries<number>(); public readonly close = createSeries<number>();
+  public readonly volume = createSeries<number>(); public readonly time = createSeries<number>();
+  public readonly hl2 = createSeries<number>(); public readonly hlc3 = createSeries<number>();
   public readonly ohlc4 = createSeries<number>();
 
   public commit(bar: Bar): void {
-    this.open.push(bar.open); this.high.push(bar.high); this.low.push(bar.low);
-    this.close.push(bar.close); this.volume.push(bar.volume); this.time.push(bar.time);
-    this.hl2.push((bar.high + bar.low) / 2);
-    this.hlc3.push((bar.high + bar.low + bar.close) / 3);
+    this.open.push(bar.open); this.high.push(bar.high); this.low.push(bar.low); this.close.push(bar.close);
+    this.volume.push(bar.volume); this.time.push(bar.time);
+    this.hl2.push((bar.high + bar.low) / 2); this.hlc3.push((bar.high + bar.low + bar.close) / 3);
     this.ohlc4.push((bar.open + bar.high + bar.low + bar.close) / 4);
   }
 
   public replaceCurrent(bar: Bar): void {
     this.open.replaceCurrent(bar.open); this.high.replaceCurrent(bar.high); this.low.replaceCurrent(bar.low);
     this.close.replaceCurrent(bar.close); this.volume.replaceCurrent(bar.volume); this.time.replaceCurrent(bar.time);
-    this.hl2.replaceCurrent((bar.high + bar.low) / 2);
-    this.hlc3.replaceCurrent((bar.high + bar.low + bar.close) / 3);
+    this.hl2.replaceCurrent((bar.high + bar.low) / 2); this.hlc3.replaceCurrent((bar.high + bar.low + bar.close) / 3);
     this.ohlc4.replaceCurrent((bar.open + bar.high + bar.low + bar.close) / 4);
   }
 
