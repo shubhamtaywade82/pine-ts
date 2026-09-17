d816dadcf3c0667a08f961fd387356fdd098da51
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-17 07:51:42 +0530

    feat: expose public pine-ts API

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { PineRuntime, type PineScript, type RuntimeOptions } from "./core/runtime.js";
+export { createSeries, type Series } from "./core/series.js";
+export type { PineContext } from "./core/context.js";
+export type {
+  Bar,
+  BarState,
+  HistoricalBarsRequest,
+  MarketDataProvider,
+  PineExecutionMode,
+  StreamBarsRequest,
+  SymbolInfo,
+} from "./core/types.js";
+
+export * as ta from "./ta/index.js";
