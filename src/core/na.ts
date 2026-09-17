64683218e21e263584074bc28f29a026a883d377
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-17 09:25:41 +0530

    refactor(core): make na predicate type-safe

--- src/core/na.ts
+++ src/core/na.ts
@@ -3,7 +3,7 @@ export const na = Number.NaN;
 
 export type PineValue<T> = T | undefined;
 
-export const isNa = <T>(value: PineValue<T>): value is undefined =>
+export const isNa = (value: unknown): boolean =>
   value === undefined || (typeof value === "number" && Number.isNaN(value));
 
 export const nz = (value: number | undefined, replacement = 0): number =>
